65af6a3cdb67977a8f7e2792fcc5fb6e315b5b29
lillian <keynesiane@gmail.com>
2025-04-16 11:48:44 -0700

    add profile controllers

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,7 +1,49 @@
 const prisma = require("../db/prisma");
 
-exports.getProfiles = () => {}
+exports.getProfiles = async (req, res) => {
+    try {
+        const profiles = await prisma.profile.findMany();
+        return res.status(200).json({profiles});
+    }
+    catch(err) {
+        return res.status(500).json({message: "Could not get profile."});
+    }
+}
 
-exports.getProfile = () => {}
+exports.getProfile = async (req, res) => {
+    try {
+        const profile = await prisma.profile.findUnique({
+            where: {
+                id: parseInt(req.params.id),
+              },
+              include: {
+                user: {
+                  include: {
+                    posts: true,
+                  },
+                },
+              },
+        });
+        return res.status(200).json({profile});
+    }
+    catch(err) {
+        return res.status(500).json({message: "Could not get profile."});
+    }
+}
 
-exports.updateProfile = () => {}
+exports.updateProfile = async (req, res) => {
+    try {
+        const profile = await prisma.profile.findUnique({
+            where: {
+                id: parseInt(req.params.id),
+              },
+              data: {
+                bio: req.body.bio,
+              },
+        });
+        return res.status(200).json({profile});
+    }
+    catch(err) {
+        return res.status(500).json({message: "Could not get profile."});
+    }
+}
